75157be4752624b89152c7c9c50f8ad318ccd11b
Gareth Cary <gareth@caryfamily.co.uk>
2020-02-21 15:39:07 +0000

    cocktail Gareth

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    # same things as @dose.cocktail_id = @cocktail.id
+    if @dose.save
+    #
+     redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+   def destroy # destroy a restaurant
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,20 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+puts "deleteing all doses"
+Dose.destroy_all
+puts "deleteing all ingredients"
+Ingredient.destroy_all
+
+puts "create all"
+
+Ingredient.create(name: "lemon")
+
+Ingredient.create(name: "ice")
+
+Ingredient.create(name: "mint leaves")
+
+puts "done"
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
